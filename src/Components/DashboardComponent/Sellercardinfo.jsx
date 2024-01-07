1340de2da518826aba2664f9c1e262b5b6ca1ff9
Arista Abdurrahman <aristaabdurrahman@gmail.com>
2024-01-07 10:05:19 +0700

    Add Dashboard pages and update file

---
+++ src/Components/DashboardComponent/Sellercardinfo.jsx
@@ -0,0 +1,24 @@
+const Sellercardinfo = () => {
+    return (
+        <>
+            <div id="dashboard-cardinfo">
+                <div className="three-col-wrapper">
+                    <div className="dashboard-card-info">
+                        <h1>Jumlah Menu</h1>
+                        <h2>20</h2>
+                    </div>
+                    <div className="dashboard-card-info">
+                        <h1>Pesanan Baru</h1>
+                        <h2>20</h2>
+                    </div>
+                    <div className="dashboard-card-info">
+                        <h1>Pesanan Selesai</h1>
+                        <h2>20</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Sellercardinfo
---
+++ src/Components/DashboardComponent/Sellerprofile.jsx
@@ -0,0 +1,22 @@
+const Sellerprofile = () => {
+    return (
+        <>
+            <div id="dashboard-seller-info">
+                <div className="dashboard-seller-wrap">
+                    <img src="" alt="seller-img" width="100%" height="265" />
+                </div>
+                <div className="dashboard-seller-wrap" style={{marginLeft: "20px"}}>
+                    <h1>Rumah Makan Sederhana</h1>
+                    <h5>Jalan Kemang Pratama Raya Blok AM No.1B, RT.003/RW.001, Bojong Menteng, Kec.
+                        Rawalumbu, Kota Bksscascascsasacsacsaca</h5>
+                    <h2>Kota, Provinsi</h2>
+                    <h3>Jenis Kuliner : Roti, Minuman</h3>
+                    <h3>Range Harga</h3>
+                    <h3>Jumlah Favorite</h3>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Sellerprofile
