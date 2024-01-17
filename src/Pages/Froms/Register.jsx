c3f26f4370595e8871e1d68cc0a9b60d5be5c52c
Arista Abdurrahman <aristaabdurrahman@gmail.com>
2024-01-17 08:39:25 +0700

    new update

--- src/Pages/Froms/Register.jsx
+++ src/Pages/Froms/Register.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react"
 import axios from "axios"
 import "./form.css"
+import Signupvalidation from "../../Service/Validation/Signupvalidation"
 
 const Register = () => {
-    
-    const [Regisname, setRegisname] = useState('')
-    const [Regisemail, setRegisemail] = useState('')
-    const [Regispassword, setRegispassword] = useState('')
-    const [Regispasswordcheck, setRegispasswordcheck] = useState('')
+
+    const [RegisData, setRegisData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmpassword:''
+    })
+
+    const [error, setError] = useState({})
+
+    const inputData = (e) =>{
+        const userData = {...RegisData, [e.target.name]: [e.target.value]}
+        setRegisData(userData)
+    }
 
     const handleRegister = (event) => {
         event.preventDefault()
-        axios.post('', {
-            name: Regisname,
-            EMail: Regisemail,
-            password: Regispassword
-        }).then( res => console.log(response))
-          .catch(er => console.log(er))
+        //setError(Signupvalidation(RegisData))
     }
 
     return (
@@ -25,29 +30,37 @@ const Register = () => {
                 <div className="register-container">
                     <h1 className="title">Daftar</h1>
                     <form id="form" onSubmit={handleRegister}>
-                        <input className="form-input-ezer" 
-                        type="text" 
-                        placeholder="Username"  
-                        onChange={e => setRegisname(e.target.value)} />
+                        <input className="form-input-ezer"
+                            type="text"
+                            placeholder="Username"
+                            name="username"
+                            onChange={inputData} />
+                        {error.username && <p style={{color: "red"}}> {error.username} </p>}
 
-                        <input className="form-input-ezer" 
-                        type="email" 
-                        placeholder="Email" 
-                        onChange={e => setRegisemail(e.target.value)}/>
+                        <input className="form-input-ezer"
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            onChange={inputData} />
+                        {error.email && <p style={{color: "red"}}> {error.email} </p>}    
 
-                        <input className="form-input-ezer" 
-                        type="password" 
-                        id="password" 
-                        placeholder="Password" 
-                        onChange={e => setRegispassword(e.target.value)}/>
+                        <input className="form-input-ezer"
+                            type="password"
+                            id="password"
+                            placeholder="Password"
+                            name="password"
+                            onChange={inputData} />
+                        {error.password && <p style={{color: "red"}}> {error.password} </p>}    
 
                         <input
                             className="form-input-ezer"
                             type="password"
                             id="confirm-password"
                             placeholder="Ulangi Password"
-                            onChange={e => setRegispasswordcheck(e.target.value)}
-                        />
+                            name="confirmpassword"
+                            onChange={inputData} />
+                        {error.confirmpassword && <p style={{color: "red"}}> {error.confirmpassword} </p>}
+                            
                         <div className="show-password">
                             <input type="checkbox" id="show-password-checkbox" />
                             <label htmlFor="show-password-checkbox" className="show"
--- src/Pages/Pembayaran.jsx
+++ src/Pages/Pembayaran.jsx
@@ -1,54 +1,54 @@
-     import Nav from "../Components/Nav";
+import Nav from "../Components/Nav";
 
-     const Pembayaran = () => {
-          return (
+const Pembayaran = () => {
+     return (
           <>
                <div className="container">
                     <Nav />
                </div>
                <div className="container-pembayaran">
-     <form action>
-     <div className="row">
-          <div className="col">
-          <img className="silang" src="./assets/images/silang.png" alt /> 
-          <div className="silang-bayar">Pembayaran</div>
-          <div className="inputBox">
-               <div className="metode">Pilih Metode Pembayaran</div>
-               <span className="metode1">Lihat Semua</span>
-          </div>
-          <div className="inputBox1">
-               <img className="bca" src="./assets/bca.png" alt />
-               <div className="bcaa">BCA Virtual Account</div>
-               <img className="metode2" src="./assets/bunder1.png" alt />
-          </div>
-          <div className="inputBox1">
-               <img className="bca" src="./assets/bri.png" alt />
-               <div className="bcaa">BRI Virtual Account</div>
-               <img className="metode2" src="./assets/bunder2.png" alt />
-          </div>
-          <div className="pem">
-               <h4>Detail Pembayaran</h4>
-               <p>Nasi Goreng (1) 
-               <span className="metode3">20.000</span>
-               </p>
-               <p>Es Jeruk (1)
-               <span className="metode4">20.000</span>
-               </p>
-          </div>
-          <p>Total
-               <span className="metode5">40.000</span>
-          </p>
-          </div>
-     </div>
-     <input type="submit" defaultValue="pesan" className="submit-btn" />
-     </form>
-     </div>
+                    <form action>
+                         <div className="row">
+                              <div className="col">
+                                   <img className="silang" src="./assets/images/silang.png" alt />
+                                   <div className="silang-bayar">Pembayaran</div>
+                                   <div className="inputBox">
+                                        <div className="metode">Pilih Metode Pembayaran</div>
+                                        <span className="metode1">Lihat Semua</span>
+                                   </div>
+                                   <div className="inputBox1">
+                                        <img className="bca" src="./assets/bca.png" alt />
+                                        <div className="bcaa">BCA Virtual Account</div>
+                                        <img className="metode2" src="./assets/bunder1.png" alt />
+                                   </div>
+                                   <div className="inputBox1">
+                                        <img className="bca" src="./assets/bri.png" alt />
+                                        <div className="bcaa">BRI Virtual Account</div>
+                                        <img className="metode2" src="./assets/bunder2.png" alt />
+                                   </div>
+                                   <div className="pem">
+                                        <h4>Detail Pembayaran</h4>
+                                        <p>Nasi Goreng (1)
+                                             <span className="metode3">20.000</span>
+                                        </p>
+                                        <p>Es Jeruk (1)
+                                             <span className="metode4">20.000</span>
+                                        </p>
+                                   </div>
+                                   <p>Total
+                                        <span className="metode5">40.000</span>
+                                   </p>
+                              </div>
+                         </div>
+                         <input type="submit" defaultValue="pesan" className="submit-btn" />
+                    </form>
+               </div>
 
 
-               
-               
+
+
           </>
-          )
-     }
+     )
+}
 
-     export default Pembayaran
+export default Pembayaran
