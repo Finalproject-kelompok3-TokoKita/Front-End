d76d538d4cb5c8b697a99f9ca0028c26cc497b5f
Agung firmansyah <agungfirmansyah1230@gmail.com>
2024-01-28 21:55:03 +0700

    edit style dibagian cart

--- src/Components/DashboardComponent/Sellermenulist.jsx
+++ src/Components/DashboardComponent/Sellermenulist.jsx
@@ -1,11 +1,17 @@
 const Sellermenulist = ({ product }) => {
-    return (
-        <>
-            <div id="dashboard-allmenu">
-                <h2 style={{ textAlign: "center" }}>Daftar Menu</h2>
-                <form action="" method="get" style={{ marginBottom: "20px" }}>
-                    <input type="text" name="" id="" className="dasboard-filter-form" placeholder="Cari produk" />
-                    {/* <select name="" id="" className="dasboard-filter-form" style={{ margin: "0 16px" }}>
+  return (
+    <>
+      <div id="dashboard-allmenu">
+        <h2 style={{ textAlign: "center" }}>Daftar Menu</h2>
+        <form action="" method="get" style={{ marginBottom: "20px" }}>
+          <input
+            type="text"
+            name=""
+            id=""
+            className="dasboard-filter-form"
+            placeholder="Cari produk"
+          />
+          {/* <select name="" id="" className="dasboard-filter-form" style={{ margin: "0 16px" }}>
                         <option value="">Makanan</option>
                         <option value="">Jajanan</option>
                         <option value="">Minuman</option>
@@ -17,35 +23,47 @@ const Sellermenulist = ({ product }) => {
                         <option value="">dst</option>
                         <option value="">dst</option>
                     </select> */}
-                </form>
-                <hr />
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Nama Menu</th>
-                            <th>Harga</th>
-                            <th>Qty</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            product.map((prod) => (
-                                <tr key={prod.id}>
-                                    <td>{prod.name}</td>
-                                    <td>{prod.price}</td>
-                                    <td>{prod.quantity}</td>
-                                    <td><button type="button" className="dashboard-allmenu-action-button"
-                                        style={{ backgroundColor: "lightseagreen" }}>Ubah</button></td>
-                                    <td><button type="button" className="dashboard-allmenu-action-button"
-                                        style={{ backgroundColor: "red" }}>Hapus</button></td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
-}
+        </form>
+        <hr />
+        <table>
+          <thead>
+            <tr>
+              <th className="fw">Nama Menu</th>
+              <th className="fw">Harga</th>
+              <th className="fw">Qty</th>
+            </tr>
+          </thead>
+          <tbody>
+            {product.map((prod) => (
+              <tr key={prod.id}>
+                <td>{prod.name}</td>
+                <td>{prod.price}</td>
+                <td>{prod.quantity}</td>
+                <td>
+                  <button
+                    type="button"
+                    className="dashboard-allmenu-action-button"
+                    style={{ backgroundColor: "lightseagreen" }}
+                  >
+                    Ubah
+                  </button>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    className="dashboard-allmenu-action-button"
+                    style={{ backgroundColor: "red" }}
+                  >
+                    Hapus
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-export default Sellermenulist
+export default Sellermenulist;
